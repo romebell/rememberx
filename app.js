f4cdda287d5cc5d0a4241a1bc8fb3f52e5c1668b
mesona <kylemesona@gmail.com>
2019-03-09 19:03:10 -0800

    Add express router for initial user route

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require("express");
-const app = express();
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
 
 app.get("/", (req, res) => res.send("Hello Rome"));
-const db = require('./config/keys').monogoURI;
+const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
