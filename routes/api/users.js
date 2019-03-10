9f537826214d4bb93bb4a79a91952553596b5ef6
mesona <kylemesona@gmail.com>
2019-03-09 19:27:16 -0800

    Add user login to /api/users/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 
 
@@ -32,6 +34,38 @@ router.post('/register', (req, res) => {
           })
         })
       }
+    })
+})
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({email: 'This user does not exist'});
+      }
+
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if (isMatch) {
+          const payload = {id: user.id, name: user.name};
+
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {expiresIn: 3600},
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            });
+        } else {
+          return res.status(400).json({password: 'Incorrect password'});
+        }
+      })
     })
 })
 
