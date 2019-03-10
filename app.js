64bebd4ad12b0e67a9cee889aca9779f7a35cc05
mesona <kylemesona@gmail.com>
2019-03-09 19:21:44 -0800

    Add user POST to /api/users/register

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
-const users = require("./routes/api/users");
 const express = require("express");
 const bodyParser = require('body-parser');
 
+const users = require("./routes/api/users");
+const db = require('./config/keys').mongoURI;
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Rome"));
-const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err))
 
+// app.get("/", (req, res) => res.send("Hello Rome"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
