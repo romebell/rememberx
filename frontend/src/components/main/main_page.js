32d4901c418815474dc0f80625a9c6be24d00023
ahmed ali <loona.ahmed@gmail.com>
2019-03-11 15:33:12 -0700

    add css for homepage

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+ 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Remembrrr</h1>
-        <footer>
+      <div className="main-page">
+        <div className="main-page-description">Remembrrr is an application based on the spaced repetition learning technique. Learners review information at gradually increasing intervals for enhanced memory</div>
+        
+        {/* <footer className="main-page-footer">
           Footer Text
-        </footer>
+        </footer> */}
       </div>
     );
   }
