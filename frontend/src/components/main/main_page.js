61a2948865bc02778be84f9102bf0fc3c78094f8
ahmed ali <loona.ahmed@gmail.com>
2019-03-11 16:17:12 -0700

    css for main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -6,10 +6,9 @@ class MainPage extends React.Component {
     return (
       <div className="main-page">
         <div className="main-page-description">Remembrrr is an application based on the spaced repetition learning technique. Learners review information at gradually increasing intervals for enhanced memory</div>
-        
-        {/* <footer className="main-page-footer">
-          Footer Text
-        </footer> */}
+
+        <button id="main-page-get-started">Get Started</button>
+
       </div>
     );
   }
