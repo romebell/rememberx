9a688712ef29ee9b7880684b58ca786cf7ddb3f9
Rome Bell <romebell@me.com>
2019-03-12 11:31:35 -0700

    add username to props and user_api_utils

--- frontend/src/greeting/greeting.jsx
+++ frontend/src/greeting/greeting.jsx
@@ -16,7 +16,7 @@ const Greeting = ({ currentUser, logout, openModal }) => {
   const personalGreeting = () => {
     return (
         <hgroup className="header-group">
-        <h2 className="header-name">Hi, {currentUser.username}!</h2>
+        <h2 className="header-name">Hi, {currentUser.name}!</h2>
         <button className="header-button" onClick={logout}>Log Out</button>
       </hgroup>
     )
--- routes/api/users.js
+++ routes/api/users.js
@@ -78,7 +78,7 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            const payload = {id: user.id, name: user.name};
+            const payload = {id: user.id, name: user.username};
 
             jwt.sign(
               payload,
