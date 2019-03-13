1c59f831521bc111773ef62ee1e8b5a3880724a4
mesona <kylemesona@gmail.com>
2019-03-12 17:29:45 -0700

    Fix login bug

--- frontend/src/greeting/greeting.jsx
+++ frontend/src/greeting/greeting.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { isEmpty } from 'lodash';
 
 
 
 const Greeting = ({ currentUser, logout, openModal }) => {
 
   const sessionLinks = () => (
-    <div className="navbar">
+    <div className="navbar" onClick={() => console.log(currentUser)}>
       <div className="logo">
         <div className="logo-image"></div>
-        <div className="logo-text">REMEMBRRR</div>
+        <div className="logo-text" onClick={() => console.log(currentUser)}>REMEMBRRR</div>
       </div>
       <nav className="navbar-links">
         <button className="signup-link" onClick={() => openModal('signup')}>Signup</button>
@@ -19,7 +20,7 @@ const Greeting = ({ currentUser, logout, openModal }) => {
   );
   const personalGreeting = () => {
     return (
-        <hgroup className="header-group">
+        <hgroup className="header-group" >
         <h2 className="header-name">Hi, {currentUser.name}!</h2>
         <button className="header-button" onClick={logout}>Log Out</button>
       </hgroup>
@@ -27,7 +28,7 @@ const Greeting = ({ currentUser, logout, openModal }) => {
   };
 
   return (
-    currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
+    !isEmpty(currentUser) ? personalGreeting(currentUser, logout) : sessionLinks()
   )
 
 };
