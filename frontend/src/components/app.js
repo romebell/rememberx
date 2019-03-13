b5eafd163c9c49a2107a2eb84c7f234b09c79461
mesona <kylemesona@gmail.com>
2019-03-13 11:08:19 -0700

    Add cursor: pointer to buttons

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,7 @@ import Modal from './modal/modal';
 import GreetingContainer from '../greeting/greeting_container';
 import { Switch, Redirect } from 'react-router-dom';
 
-import MainPage from './main/main_page';
+import MainPageContainer from './main/main_page_container';
 
 import NoteIndexContainer from './note/note_index_container';
 import NoteFormContainer from './note/note_form_container';
@@ -16,7 +16,7 @@ const App = () => (
     <Modal />
     <GreetingContainer />
     <Switch>
-        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/" component={MainPageContainer} />
         <ProtectedRoute path="/notes/new" component={NoteFormContainer} />
         <ProtectedRoute path="/notes/:noteId" component={NoteIndexContainer} />
         <ProtectedRoute path="/notes" component={NoteIndexContainer} />
